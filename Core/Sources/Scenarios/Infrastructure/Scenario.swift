a982480a26c70777a96bde7066e6d49023d86688
Toby O'Connell <tobyoconnell@hotmail.com>
2021-03-28 02:17:02 +0100

    Create Scenario type

---
+++ Core/Sources/Scenarios/Infrastructure/Scenario.swift
@@ -0,0 +1,13 @@
+//
+//  Scenario.swift
+//  
+//
+//  Created by Toby O'Connell on 27/03/2021.
+//
+
+import Integration
+
+protocol Scenario: IdentifiableType {
+    static var path: Path { get }
+    static var coordinator: Coordinating { get }
+}
