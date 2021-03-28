56e7dd5cd7899c5626dc3874b413cbc196ef4ea1
Toby O'Connell <tobyoconnell@hotmail.com>
2021-03-28 11:10:20 +0100

    Create entry points for Scenarios and `real` app

---
+++ UserDataCollection/Sources/Internal/AppCoordinator.swift
@@ -0,0 +1,10 @@
+//
+//  AppCoordinator.swift
+//  UserDataCollection
+//
+//  Created by Toby O'Connell on 28/03/2021.
+//
+
+import Scenarios
+
+typealias AppCoordinator = ScenarioCoordinator
---
+++ UserDataCollection/Sources/Producation/AppCoordinator.swift
@@ -0,0 +1,10 @@
+//
+//  AppCoordinator.swift
+//  UserDataCollection
+//
+//  Created by Toby O'Connell on 28/03/2021.
+//
+
+import Integration
+
+typealias AppCoordinator = RootCoordinator
