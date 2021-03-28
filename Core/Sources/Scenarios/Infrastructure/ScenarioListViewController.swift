e9979618b828390e6c7e465d10c7249fe5976384
Toby O'Connell <tobyoconnell@hotmail.com>
2021-03-28 10:57:52 +0100

    Add ScenarioListViewController
    - Presents all Scenarios for selection

---
+++ Core/Sources/Scenarios/Infrastructure/ScenarioListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ScenarioListViewController.swift
+//  
+//
+//  Created by Toby O'Connell on 28/03/2021.
+//
+
+import UIKit
+
+class ScenarioListViewController: HierarchyViewController {
+    init(select: @escaping (ScenarioID) -> Void) throws {
+        let items: [Item] = try ScenarioID.allCases
+            .reduce(into: [Item]()) { (items, scenarioID) in
+                let select = { select(scenarioID) }
+                try items.insert(scenarioID.scenarioType.path, action: select)
+            }
+        super.init(items: items)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
