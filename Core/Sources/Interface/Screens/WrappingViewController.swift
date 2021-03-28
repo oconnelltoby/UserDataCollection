df54de0ce8b295bfefdc7a1c9f8a3bfd8ffa1380
Toby O'Connell <tobyoconnell@hotmail.com>
2021-03-28 02:14:53 +0100

    Create WrappingViewController
    - This handles transparently swapping view controller content

---
+++ Core/Sources/Interface/Screens/WrappingViewController.swift
@@ -0,0 +1,36 @@
+//
+//  WrappingViewController.swift
+//  
+//
+//  Created by Toby O'Connell on 27/03/2021.
+//
+
+import UIKit
+
+public class WrappingViewController: UIViewController {
+    public var content: UIViewController? {
+        didSet {
+            oldValue.map(remove)
+            content.map(add)
+            UIAccessibility.post(notification: .screenChanged, argument: nil)
+        }
+    }
+    
+    public override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    public override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
