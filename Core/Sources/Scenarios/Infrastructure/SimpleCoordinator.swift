078dc991ddaca00aaa33b2b945ed226c121e82fd
Toby O'Connell <tobyoconnell@hotmail.com>
2021-03-28 02:18:15 +0100

    Create SimpleCoordinator
    - Conveniently wraps a single view controller in a coordinator

---
+++ Core/Sources/Scenarios/Infrastructure/SimpleCoordinator.swift
@@ -0,0 +1,23 @@
+//
+//  SimpleCoordinator.swift
+//  
+//
+//  Created by Toby O'Connell on 28/03/2021.
+//
+
+import Integration
+import UIKit
+
+class SimpleCoordinator: Coordinating {
+    let navigationController = UINavigationController()
+    
+    private var rootViewController: UIViewController
+    
+    init(rootViewController: UIViewController) {
+        self.rootViewController = rootViewController
+    }
+    
+    func start() {
+        navigationController.pushViewController(rootViewController, animated: false)
+    }
+}
