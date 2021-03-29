448aacba298b80abe67c32c454896200d8add995
Toby O'Connell <tobyoconnell@hotmail.com>
2021-03-29 21:26:02 +0100

    Add primary button styling
    Also added a convenience function for adding closure actions

---
+++ Core/Sources/Interface/Style/Extensions/UIButton.swift
@@ -0,0 +1,44 @@
+//
+//  UIButton.swift
+//  
+//
+//  Created by Toby O'Connell on 28/03/2021.
+//
+
+import UIKit
+
+extension UIButton {
+    @discardableResult
+    public func styleAsPrimary() -> Self {
+        layer.cornerRadius = .buttonCornerRadius
+        tintColor = UIColor(.primaryButtonText)
+        titleLabel?.styleAsDyanmicText(.headline)
+        titleLabel?.textAlignment = .center
+        
+        setTitleColor(.init(.primaryButtonText), for: .normal)
+                
+        let gradientView = GradientView()
+        gradientView.translatesAutoresizingMaskIntoConstraints = false
+        insertSubview(gradientView, at: 0)
+        clipsToBounds = true
+        
+        NSLayoutConstraint.activate([
+            heightAnchor.constraint(greaterThanOrEqualToConstant: .buttonMinimumHeight),
+            heightAnchor.constraint(greaterThanOrEqualTo: titleLabel!.heightAnchor, constant: .halfSpacing * 2),
+            widthAnchor.constraint(greaterThanOrEqualTo: titleLabel!.widthAnchor, constant: .standardSpacing * 2),
+            
+            gradientView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            gradientView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            gradientView.topAnchor.constraint(equalTo: topAnchor),
+            gradientView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+        
+        return self
+    }
+    
+    @discardableResult
+    func addAction(_ action: @escaping () -> Void) -> Self {
+        addAction(.init(handler: { _ in action() }), for: .touchUpInside)
+        return self
+    }
+}
