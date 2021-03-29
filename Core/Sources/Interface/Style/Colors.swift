faf05cfd1f211a17c3e51dd02a58273bac25c0b7
Toby O'Connell <tobyoconnell@hotmail.com>
2021-03-29 21:21:21 +0100

    Add button + background colors

---
+++ Core/Sources/Interface/Style/Colors.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  
+//
+//  Created by Toby O'Connell on 28/03/2021.
+//
+
+import UIKit
+
+enum ColorName: String, CaseIterable {
+    case accentGreen = "AccentGreen"
+    case accentTeal = "AccentTeal"
+    case primaryButtonText = "PrimaryButtonText"
+    case background = "Background"
+}
+
+extension UIColor {
+    static var bundle: Bundle = Bundle.main
+    
+    convenience init(_ name: ColorName) {
+        self.init(named: name.rawValue)!
+    }
+    
+    static func hasColor(for name: ColorName) -> Bool {
+        Self(named: name.rawValue) != nil
+    }
+    
+    private convenience init(named name: String) {
+        self.init(named: name, in: Self.bundle, compatibleWith: .current)!
+    }
+}
