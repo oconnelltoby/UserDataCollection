a7f190cbe9be36daeb241ddfab7cc9d3a24210a9
Toby O'Connell <tobyoconnell@hotmail.com>
2021-03-29 21:25:12 +0100

    Add common dimensions

---
+++ Core/Sources/Interface/Style/Dimensions.swift
@@ -0,0 +1,16 @@
+//
+//  Dimensions.swift
+//  
+//
+//  Created by Toby O'Connell on 28/03/2021.
+//
+
+import CoreGraphics
+
+extension CGFloat {
+    static let standardSpacing: CGFloat = 16
+    static let halfSpacing: CGFloat = standardSpacing / 2
+
+    static let buttonCornerRadius: CGFloat = 15
+    static let buttonMinimumHeight: CGFloat = 45
+}
