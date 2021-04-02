68d96ca297c4c9f9b7138b964cb73a8acc7a69c3
Toby O'Connell <tobyoconnell@hotmail.com>
2021-04-02 21:26:49 +0100

    Add AppController protocol
    - Acts as a proxy between SceneDelegate and UI

---
+++ Core/Sources/Integration/AppController.swift
@@ -0,0 +1,12 @@
+//
+//  AppController.swift
+//  
+//
+//  Created by Toby O'Connell on 02/04/2021.
+//
+
+import UIKit
+
+public protocol AppController {
+    var rootViewController: UIViewController { get }
+}
