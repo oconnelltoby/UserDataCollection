b3bacea8a9dfdae23758b8ed467a526be9f758cc
Toby O'Connell <tobyoconnell@hotmail.com>
2021-04-03 10:12:14 +0100

    Add helper for showing alerts on UIViewController
    - Useful for UI testing to verify actions have been triggered

---
+++ Core/Sources/Scenarios/Infrastructure/Extensions/UIViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewController.swift
+//  
+//
+//  Created by Toby O'Connell on 31/03/2021.
+//
+
+import UIKit
+
+extension UIViewController {
+    func showAlert(title: String, message: String? = nil, actionTitle: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: actionTitle, style: .default)
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+}
