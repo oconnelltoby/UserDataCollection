cbf09f36cbb569b4ea8c011694bf27955c9cbeea
Toby O'Connell <tobyoconnell@hotmail.com>
2021-04-10 10:03:29 +0100

    Add edge inset dimensions

--- Core/Sources/Interface/Style/Dimensions.swift
+++ Core/Sources/Interface/Style/Dimensions.swift
@@ -2,7 +2,7 @@
 // Copyright © 2021 Toby O'Connell. All rights reserved.
 //
 
-import CoreGraphics
+import UIKit
 
 public extension CGFloat {
     static let standardSpacing: CGFloat = 16
@@ -11,4 +11,13 @@ public extension CGFloat {
     static let buttonCornerRadius: CGFloat = 15
     static let borderWidth: CGFloat = 2
     static let buttonMinimumHeight: CGFloat = 45
+}
+
+extension UIEdgeInsets {
+    static let halfSpacing = UIEdgeInsets(
+        top: .halfSpacing,
+        left: .halfSpacing,
+        bottom: .halfSpacing,
+        right: .halfSpacing
+    )
 }
