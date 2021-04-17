c00bd90d0191f70e00dd93384779e78299b8baa2
Toby O'Connell <tobyoconnell@hotmail.com>
2021-04-17 10:23:07 +0100

    Add convenience function for creating system icons

---
+++ Core/Sources/Interface/Extensions/UIImageView.swift
@@ -0,0 +1,19 @@
+//
+// Copyright © 2021 Toby O'Connell. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+    static func icon(systemName: String) -> UIImageView {
+        let configuration = UIImage.SymbolConfiguration(textStyle: .title2)
+        let image = UIImage(systemName: systemName, withConfiguration: configuration)?
+            .withTintColor(.init(.primaryText), renderingMode: .alwaysOriginal)
+        let imageView = UIImageView(image: image)
+
+        imageView.setContentHuggingPriority(.required, for: .horizontal)
+        imageView.setContentHuggingPriority(.required, for: .vertical)
+
+        return imageView
+    }
+}
