a7021f086986a4d870b04168519757983b5a2ffe
Toby O'Connell <tobyoconnell@hotmail.com>
2021-04-18 10:19:32 +0100

    Add Resources to Localization package

--- Core/Package.swift
+++ Core/Package.swift
@@ -31,7 +31,8 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "Localization"
+            name: "Localization",
+            resources: [.process("Resources")]
         ),
         .target(
             name: "Interface",
