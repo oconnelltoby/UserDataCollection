d5f3a3045afadf59b7972d1378047d918da0e2a8
Toby O'Connell <tobyoconnell@hotmail.com>
2021-04-25 17:45:25 +0100

    Allow TextFieldDelegate to cap text length

--- Core/Sources/Interface/Delegate/TextFieldDelegate.swift
+++ Core/Sources/Interface/Delegate/TextFieldDelegate.swift
@@ -6,15 +6,27 @@ import UIKit
 
 class TextFieldDelegate: NSObject, UITextFieldDelegate {
     var shouldReturn: (UITextField) -> Bool = { _ in true }
+    var shouldChange: (UITextField, NSRange, String) -> Bool = { _, _, _ in true }
 
-    init(shouldReturn: ((UITextField) -> Bool)? = nil) {
+    init(
+        shouldReturn: ((UITextField) -> Bool)? = nil,
+        shouldChange: ((UITextField, NSRange, String) -> Bool)? = nil
+    ) {
         if let shouldReturn = shouldReturn {
             self.shouldReturn = shouldReturn
+        }
+
+        if let shouldChange = shouldChange {
+            self.shouldChange = shouldChange
         }
     }
 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         shouldReturn(textField)
+    }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        shouldChange(textField, range, string)
     }
 }
 
@@ -25,6 +37,20 @@ extension TextFieldDelegate {
         self.shouldReturn = {
             $0.resignFirstResponder()
             return shouldReturn($0)
+        }
+        return self
+    }
+
+    @discardableResult
+    func capAt(length: UInt) -> Self {
+        let shouldChange = self.shouldChange
+        self.shouldChange = { textField, range, string in
+            let text = textField.text ?? ""
+            guard let textRange = Range(range, in: text) else { return false }
+            let updatedText = text.replacingCharacters(in: textRange, with: string)
+            let isShortEnough = updatedText.count <= length
+
+            return shouldChange(textField, range, string) && isShortEnough
         }
         return self
     }
