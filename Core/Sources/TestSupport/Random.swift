193101c126d3e8e306b50eca3aae90930a4594c5
Toby O'Connell <tobyoconnell@hotmail.com>
2021-04-25 17:42:26 +0100

    Add random string function with length

--- Core/Sources/TestSupport/Random.swift
+++ Core/Sources/TestSupport/Random.swift
@@ -8,4 +8,14 @@ public extension String {
     static func random() -> String {
         UUID().uuidString
     }
+
+    static func random(length: Int) -> String {
+        var letters = String()
+        return (0 ..< length).reduce(into: "") { string, _ in
+            if letters.isEmpty {
+                letters = String.random()
+            }
+            letters.popLast().map { string.append($0) }
+        }
+    }
 }
