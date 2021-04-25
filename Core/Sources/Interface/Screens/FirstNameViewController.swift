df57fbe247279c673ea936bc046b3229a93e5f75
Toby O'Connell <tobyoconnell@hotmail.com>
2021-04-25 11:34:32 +0100

    Refactor FirstNameViewController + Scenario

--- Core/Sources/Interface/Screens/FirstNameViewController.swift
+++ Core/Sources/Interface/Screens/FirstNameViewController.swift
@@ -4,24 +4,47 @@
 
 import Localization
 import UIKit
+import UserDataValidation
+
+public struct FirstNameViewModel {
+    var minLength: UInt
+    var maxLength: UInt
+    var validate: (_ name: String) -> Result<Void, Error>
+
+    public init(minLength: UInt, maxLength: UInt, validate: @escaping (_ name: String) -> Result<Void, Error>) {
+        self.minLength = minLength
+        self.maxLength = maxLength
+        self.validate = validate
+    }
+}
 
 public class FirstNameViewController: UIViewController {
     let textFieldDelegate = TextFieldDelegate().resignOnReturn()
 
-    public init() {
+    public init(viewModel: FirstNameViewModel) {
         super.init(nibName: nil, bundle: nil)
-
-        let textField = TextField(placeholder: localize(.first_name_textfield_placeholder))
-        textField.delegate = textFieldDelegate
 
         let headding = UILabel.heading(localize(.first_name_heading))
         let body = UILabel.body(localize(.first_name_body))
-        let button = UIButton.primary(localize(.first_name_button))
         let infoIcon = UIImageView.icon(systemName: "info.circle")
-        let infoLabel = UILabel.body(localize(.first_name_info(min: 1, max: 50)))
+        let infoLabel = UILabel.body(localize(.first_name_info(min: viewModel.minLength, max: viewModel.maxLength)))
         let infoStack = UIStackView(arrangedSubviews: [infoIcon, infoLabel]).styleAsRow()
+        let textField = TextField(placeholder: localize(.first_name_textfield_placeholder)).withDelegate(textFieldDelegate)
+        let button = UIButton.primary(localize(.first_name_button)).addAction {
+            switch viewModel.validate(textField.text ?? "") {
+            case .success:
+                textField.resignFirstResponder()
+            case .failure:
+                textField.errorState = .error
+            }
+        }
 
-        view = TemplateView(scrolling: [headding, body, textField, infoStack], footer: button)
+        let action = UIAction { [weak textField] _ in
+            textField?.errorState = .normal
+        }
+        textField.addAction(action, for: .editingChanged)
+
+        view = TemplateView(scrolling: [headding, body, infoStack, textField], footer: button)
         title = localize(.first_name_title)
     }
 
--- Core/Sources/Scenarios/Screens/FirstNameScreenScenario.swift
+++ Core/Sources/Scenarios/Screens/FirstNameScreenScenario.swift
@@ -4,8 +4,52 @@
 
 import Integration
 import Interface
+import UIKit
+import UserDataValidation
+import Common
 
-class FirstNameScreenScenario: Scenario {
-    static let path = Path("Prototype Screens", "First Name")
-    static let coordinator: Coordinating = SimpleCoordinator(rootViewController: FirstNameViewController())
+protocol FirstNameScreenScenario: Scenario {
+    static var result: Result<Void, Error> { get }
+}
+
+extension FirstNameScreenScenario {
+    static var minLength: UInt { 2 }
+    static var maxLength: UInt { 10 }
+
+    static func alertTitle(for name: String) -> String {
+        "\(name) submitted"
+    }
+
+    static var coordinator: Coordinating {
+        var showAlert: ((String) -> Void)?
+
+        let viewController = FirstNameViewController(
+            viewModel: .init(
+                minLength: minLength,
+                maxLength: maxLength,
+                validate: {
+                    showAlert?($0)
+                    return result
+                }
+            )
+        )
+
+        showAlert = { [weak viewController] in
+            let title = alertTitle(for: $0)
+            viewController?.showAlert(title: title, actionTitle: "Ok")
+        }
+
+        return SimpleCoordinator(rootViewController: viewController)
+    }
+}
+
+class FirstNameSuccessScreenScenario: FirstNameScreenScenario {
+    static var path = Path("Prototype Screens", "First Name", "Success")
+    static let result: Result<Void, Error> = .success
+}
+
+class FirstNameFailureScreenScenario: FirstNameScreenScenario {
+    static var path = Path("Prototype Screens", "First Name", "Failure")
+    struct TestError: Error {}
+    static let result: Result<Void, Error> = .failure(TestError())
 }
