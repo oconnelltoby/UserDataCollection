36d6b4ea185e237f3317de33ab6d18af9ca1b834
Toby O'Connell <tobyoconnell@hotmail.com>
2021-05-02 11:03:13 +0100

    Add MockLastNameValidator

---
+++ Core/Tests/IntegrationTests/Mocks/MockLastName.swift
@@ -0,0 +1,9 @@
+//
+// Copyright © 2021 Toby O'Connell. All rights reserved.
+//
+
+import Integration
+
+struct MockLastName: LastNameModel {
+    var value: String
+}
---
+++ Core/Tests/IntegrationTests/Mocks/MockLastNameValidator.swift
@@ -0,0 +1,24 @@
+//
+// Copyright © 2021 Toby O'Connell. All rights reserved.
+//
+
+import Integration
+import UserDataValidation
+
+struct MockLastNameValidator: LastNameValidating {
+    var validInput: String
+    var tooLongInput: String
+    var tooShortInput: String
+    var nameLength: ClosedRange<UInt>
+
+    func makeLastName(input: String) -> Result<LastNameModel, LastNameValidator.LastNameError> {
+        switch input {
+        case tooLongInput:
+            return .failure(.tooLong)
+        case tooShortInput:
+            return .failure(.tooShort)
+        default:
+            return .success(MockLastName(value: input))
+        }
+    }
+}
