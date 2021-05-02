a17079a81194f03c49fdd4f2ba0ab107d93cfe9c
Toby O'Connell <tobyoconnell@hotmail.com>
2021-05-02 09:11:34 +0100

    Add convenience functions for localized labels

--- Core/Sources/Interface/Extensions/UILabel.swift
+++ Core/Sources/Interface/Extensions/UILabel.swift
@@ -2,6 +2,7 @@
 // Copyright © 2021 Toby O'Connell. All rights reserved.
 //
 
+import Localization
 import UIKit
 
 public extension UILabel {
@@ -30,12 +31,32 @@ public extension UILabel {
         accessibilityTraits.insert(.header)
         return self
     }
+}
 
+extension UILabel {
     static func body(_ text: String) -> UILabel {
         UILabel().styleAsDyanmicText(.body).addText(text)
     }
 
+    static func body(_ key: LocalizationKey) -> UILabel {
+        .body(localize(key))
+    }
+
+    static func body(_ key: ParameterizedLocalizationKey) -> UILabel {
+        .body(localize(key))
+    }
+}
+
+extension UILabel {
     static func heading(_ text: String) -> UILabel {
         UILabel().styleAsDyanmicText(.title1).styleAsHeader().addText(text)
+    }
+    
+    static func heading(_ key: LocalizationKey) -> UILabel {
+        .heading(localize(key))
+    }
+
+    static func heading(_ key: ParameterizedLocalizationKey) -> UILabel {
+        .heading(localize(key))
     }
 }
