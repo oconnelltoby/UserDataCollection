ddb9b1f8cbec18761b5a44b61c101bec9a82e3be
Toby O'Connell <tobyoconnell@hotmail.com>
2021-05-02 14:42:14 +0100

    Make user data CustomDebugStringConvertible

--- Core/Sources/Domain/UserData.swift
+++ Core/Sources/Domain/UserData.swift
@@ -4,21 +4,29 @@
 
 import UserDataValidation
 
-public struct FirstNameUserData: Passthrough {
+public struct FirstNameUserData: Passthrough, CustomDebugStringConvertible {
     public var previous: Void = ()
     public var firstName: FirstNameModel
     
     public init(firstName: FirstNameModel) {
         self.firstName = firstName
     }
+
+    public var debugDescription: String {
+        "firstName: \(firstName.value)"
+    }
 }
 
-public struct LastNameUserData: Passthrough {
+public struct LastNameUserData: Passthrough, CustomDebugStringConvertible {
     public var previous: FirstNameUserData
     public var lastName: LastNameModel
     
     public init(lastName: LastNameModel, previous: FirstNameUserData) {
         self.lastName = lastName
         self.previous = previous
+    }
+    
+    public var debugDescription: String {
+        "\(previous), lastName: \(lastName.value)"
     }
 }
