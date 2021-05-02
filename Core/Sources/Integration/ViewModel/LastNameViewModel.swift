839acf955ab3f6873f0bd1deb09fded8c34094a6
Toby O'Connell <tobyoconnell@hotmail.com>
2021-05-02 11:03:36 +0100

    Add adaptor for LastNameViewModel

---
+++ Core/Sources/Integration/ViewModel/LastNameViewModel.swift
@@ -0,0 +1,26 @@
+//
+// Copyright © 2021 Toby O'Connell. All rights reserved.
+//
+
+import Common
+import Interface
+import UserDataValidation
+
+extension LastNameViewModel {
+    init(validator: LastNameValidating, store: @escaping (LastNameModel) -> Void) {
+        self.init(
+            minLength: validator.nameLength.lowerBound,
+            maxLength: validator.nameLength.upperBound,
+            validate: { input in
+                let result = validator.makeLastName(input: input)
+                switch result {
+                case let .success(firstName):
+                    store(firstName)
+                    return .success
+                case let .failure(error):
+                    return .failure(error)
+                }
+            }
+        )
+    }
+}
---
+++ Core/Tests/IntegrationTests/ViewModel/LastNameViewModelTests.swift
@@ -0,0 +1,71 @@
+//
+// Copyright © 2021 Toby O'Connell. All rights reserved.
+//
+
+import UserDataValidation
+import XCTest
+@testable import Integration
+@testable import Interface
+
+class LastNameViewModelTests: XCTestCase {
+    var validator: MockLastNameValidator!
+    var viewModel: LastNameViewModel!
+    var storedLastName: LastNameModel?
+
+    override func setUp() {
+        validator = MockLastNameValidator(
+            validInput: "Valid Last Name",
+            tooLongInput: "Too Long Last Name",
+            tooShortInput: "Too Short Last Name",
+            nameLength: 0 ... .max
+        )
+
+        viewModel = LastNameViewModel(
+            validator: validator,
+            store: { [weak self] in
+                self?.storedLastName = $0
+            }
+        )
+
+        storedLastName = nil
+    }
+
+    func testMinLengthMatchesValidator() {
+        XCTAssertEqual(viewModel.minLength, validator.nameLength.lowerBound)
+    }
+
+    func testMaxLengthMatchesValidator() {
+        XCTAssertEqual(viewModel.maxLength, validator.nameLength.upperBound)
+    }
+
+    func testValidInputIsStored() {
+        _ = viewModel.validate(validator.validInput)
+        XCTAssertEqual(storedLastName?.value, validator.validInput)
+    }
+
+    func testTooLongInputIsNotStored() {
+        _ = viewModel.validate(validator.tooLongInput)
+        XCTAssertNil(storedLastName?.value)
+    }
+
+    func testTooShortInputIsNotStored() {
+        _ = viewModel.validate(validator.tooShortInput)
+        XCTAssertNil(storedLastName?.value)
+    }
+
+    func testValidInputReturnsSuccess() {
+        XCTAssertNoThrow(try viewModel.validate(validator.validInput).get())
+    }
+
+    func testTooLongInputReturnsTooLongError() {
+        XCTAssertThrowsError(try viewModel.validate(validator.tooLongInput).get()) { error in
+            XCTAssertEqual(error as? LastNameValidator.LastNameError, .tooLong)
+        }
+    }
+
+    func testTooShortInputReturnsTooShortError() {
+        XCTAssertThrowsError(try viewModel.validate(validator.tooShortInput).get()) { error in
+            XCTAssertEqual(error as? LastNameValidator.LastNameError, .tooShort)
+        }
+    }
+}
